2b35cfcc31de554382da666598055d804f8ef263
Aoi-Kobayashi <oosabaaoi@gmail.com>
2025-07-21 13:33:54 +0900

    修正

--- List2/DetailView.swift
+++ List2/DetailView.swift
@@ -6,13 +6,41 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct DetailView: View {
+    @State var animal: Animal
+    @State var player: AVAudioPlayer!
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Text(animal.name)
+                .font(.title)
+                .bold()
+            Spacer()
+            Image(animal.imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .shadow(radius: 10)
+            Spacer()
+            Button{
+                player.currentTime = 0
+                player.play()
+            } label: {
+                Text("鳴き声を聞く")
+            }
+            .buttonStyle(BorderedProminentButtonStyle())
+            Spacer()
+            Spacer()
+        }
+        .padding()
+        .onAppear {
+            player = try! AVAudioPlayer(data: animal.sound)
+        }
     }
 }
 
 #Preview {
-    DetailView()
+    DetailView(animal: .cat)
 }
