f5ea3856ffd9b59b62c5ea4b63cf3a1a1cb015e5
Aoi-Kobayashi <oosabaaoi@gmail.com>
2025-07-21 13:08:37 +0900

    Initial Commit

---
+++ List2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  List2
+//
+//  Created by AoiKobayashi on 2025/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ List2/List2App.swift
@@ -0,0 +1,17 @@
+//
+//  List2App.swift
+//  List2
+//
+//  Created by AoiKobayashi on 2025/07/21.
+//
+
+import SwiftUI
+
+@main
+struct List2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
